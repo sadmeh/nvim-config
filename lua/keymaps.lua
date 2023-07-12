bd334488d52f98a283567645e8dfdcee429892d5
U-PMERA\Mehdi.S <Mehdi.S@PMERA-L004.pmera.com>
2023-07-12 13:34:38 -0400

    like before with lua config

---
+++ lua/keymaps.lua
@@ -0,0 +1,25 @@
+require('telescope').load_extension('file_browser')
+require('telescope').load_extension('project')
+
+local wk = require("which-key")
+wk.setup {}
+
+
+wk.register({
+  ["<leader>"] = {
+    c = {
+      name = "Config",
+      r = { "<cmd>source $MYVIMRC<CR>", "Reload Config" },
+      e = { "<cmd>edit $MYVIMRC<CR>", "Edit Config" },
+    },
+    f = {
+      name = "File",
+      f = { "<cmd>Telescope find_files<CR>", "Find Files" },
+      g = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
+      b = { "<cmd>Telescope buffers<CR>", "List Buffers" },
+      h = { "<cmd>Telescope help_tags<CR>", "Help Tags" },
+      b = { "<cmd>Telescope file_browser<CR>", "File Browser" },
+      p = { "<cmd>Telescope project<CR>", "Project Picker" },
+    },
+  },
+})
